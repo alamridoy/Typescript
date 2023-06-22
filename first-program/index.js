b3b477562d834895952219a866ab1c774f29cb2b
Ridoy~ <alamridoy7@gmail.com>
2023-06-22 00:35:14 -0700

    new commit

---
+++ first-program/index.js
@@ -0,0 +1,8 @@
+var userName = "Ridoy";
+console.log(userName);
+console.log("haana");
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+add(10, 23);
+// index.js => tsc.cmd index.ts --watch => index.js => node index.js
---
+++ first-program/index.ts
@@ -0,0 +1,14 @@
+let userName = "Ridoy"
+
+console.log(userName);
+console.log("haana");
+
+
+function add(num1:number,num2:number){
+    console.log(num1+num2);
+
+}
+
+add(10,23)
+
+// index.js => tsc.cmd index.ts --watch => index.js => node index.js
