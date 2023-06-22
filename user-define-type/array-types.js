74b1dd3220757dab574411b2350175b33902dda5
Ridoy~ <alamridoy7@gmail.com>
2023-06-22 03:30:50 -0700

    array types

---
+++ user-define-type/array-types.js
@@ -0,0 +1,10 @@
+// single type
+// let userName: Array <string>
+// userName = ["abd", "dfsdsf", "dsfds"];
+// console.log(userName);
+//multiple type
+var multipleType;
+multipleType = ['ridoy', 'dhaka', 284938];
+//multipleType.sort()
+//multipleType.push("anhiata")
+console.log(multipleType);
---
+++ user-define-type/array-types.ts
@@ -0,0 +1,16 @@
+
+// single type
+// let userName: Array <string>
+// userName = ["abd", "dfsdsf", "dsfds"];
+// console.log(userName);
+
+
+//multiple type
+
+let multipleType : (string|number)[];
+
+multipleType =['ridoy','dhaka',0284938]
+//multipleType.sort()
+//multipleType.push("anhiata")
+
+console.log(multipleType);
