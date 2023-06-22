fe6b76b228ec990cb097bab70af51d040492ffd5
Ridoy~ <alamridoy7@gmail.com>
2023-06-22 02:06:50 -0700

    built in datatype

---
+++ data-type/data-type.js
@@ -0,0 +1,12 @@
+// built-in data  type : number,string,boolean,void,undefine,null
+var userId;
+var userName;
+var isActicved;
+userId = 123;
+userName = "Ridoy";
+isActicved = true;
+console.log("your id:".concat(userId, ",Your Name: ").concat(userName, ", are you activate:").concat(isActicved));
+function display() {
+    console.log("hello im display function");
+}
+display();
---
+++ data-type/data-type.ts
@@ -0,0 +1,20 @@
+// built-in data  type : number,string,boolean,void,undefine,null
+
+
+let userId : number;
+let userName : string;
+let isActicved: boolean;
+
+userId = 123;
+userName = "Ridoy"
+isActicved = true
+
+console.log(`your id:${userId},Your Name: ${userName}, are you activate:${isActicved}`);
+
+
+
+function display() : void{
+    console.log("hello im display function");
+    
+}
+display()
